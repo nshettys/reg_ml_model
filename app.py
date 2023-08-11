b21f4b8acf9cd53ccea70181f4b928f1b7f7cf6a
Nitesh <nshettys@gmail.com>
2023-08-12 01:53:19 +0530

    new commit 1

--- app.py
+++ app.py
@@ -22,10 +22,19 @@ def predict():
     print(data)
     print(np.array(list(data.values())).reshape(1,-1))
     #new_data = scaler.transform(np.array(list(data.values())).reshape(1,-1))
-    new_data = np.array(list(data.values())).reshape(1,-1)
+    new_data = np.array(list(data.values()).reshape(1,-1))
     output = regmodel.predict(new_data)
     print(output[0])
     return jsonify(output[0])
+
+@app.route('/new_predict', methods=['POST'])
+def new_predict():
+    data=[float(x) for x in request.form.values()]
+    #final_data = scaler.transform(np.array(data).reshape(1,-1))
+    final_data = np.array(data).reshape(1,-1)
+    print(final_data)
+    output = regmodel.predict(final_data)[0]
+    return render_template("home.html",prediction_text= "The predicted valued is {}".format(output))
 
 if __name__ == "__main__":
     app.run(debug= True)
